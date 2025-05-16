b29548ee95068b8aa591f5950ed5652587aa17cc
Marcos <marcosgab2006@gmail.com>
2025-05-16 15:22:41 -0300

    parte 1

--- projeto/main.py
+++ projeto/main.py
@@ -1,13 +1,14 @@
 from os import system
 from time import sleep
+from usar_gemini import gerar_roadmap
 
 system('cls')
 print('Bem vindo(a) ao Mapa de carreiras!')
-print('Descubra como se tornar um profissional em algum campo específico.')
+print('Descubra como se tornar um profissional em qualquer campo..')
 
 print("""
 [1] Escolher profissão específica 
-[2] Descubra profissões que mais combinam com você
+[2] Descubra profissões que mais combinam com você com base no seu perfil
 """)
 
 opc = input('Qual a sua opção? [1] ou [2]: ')
@@ -15,7 +16,13 @@ while opc not in '12':
       print('Por favor selecione um valor válido.')
       opc = input('Qual a sua opção? [1] ou [2]: ')
 
-if opc == 1:
-      pass
-elif opc == 2:
+if opc == '1':
+      system('cls')
+      print('Voce escolheu uma profissão específica.')
+      print('O sistema irá criar um roadmap de como alcança-la.')
+      profissao = input('Digite qual é a profissão: ')
+
+      gerar_roadmap(profissao=profissao)
+
+elif opc == '2':
       pass
--- projeto/usar_gemini.py
+++ projeto/usar_gemini.py
@@ -5,7 +5,20 @@ import os
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
+if not api_key:
+    print("Defina GEMINI_API_KEY no seu .env")
+    exit(1)
+
 genai.configure(api_key=api_key)
 
+def gerar_roadmap(profissao: str):
+      modelo = genai.GenerativeModel(model_name='gemini-2.0-flash')
 
+      prompt = f"Você é um mentor experiente e direto ao ponto. Sua missão é ajudar pessoas a alcançarem seus objetivos profissionais com clareza e eficiência, sem termos muito técnicos. Crie um roadmap resumido e direto para a carreira de {profissao}. Liste os passos principais em tópicos claros e objetivos, sem explicações longas. faca esse roadmap em formato de lista onde, sem comentarios e carcateres ou simbolos desnecessarios. Seja organizado. No final da lista, fale algumas dicas extras se possível."
+
+      roadmap = modelo.generate_content(prompt)
+
+      os.system('cls')
+      print(f'Roadmap para se tornar {profissao}: \n')
+      print(roadmap.text)
 
